7e4c7e232c81ad29f751d9f57032787f0790f388
MaheshSumb <maheshsumbpatil87@gmail.com>
2025-06-21 03:21:04 +0530

    Introduction to Hibernate and Entity Class: Basic Annotation like Entity, ID, Table,GeneratedValue, Column

---
+++ ORMIntroductionDay1/src/main/java/com/tka/ORMIntroductionEntityDay1/Student.java
@@ -0,0 +1,69 @@
+package com.tka.ORMIntroductionEntityDay1;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity  // Marks class name as an database table name
+@Table(name="StudentRecords")   // Explicitly Declare the Table name in DB 
+public class Student {
+
+	// POJO : Plain object java object
+	@Id  // Used to declare a primary key field in DB Table 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  // used implement AUTO_ICREMENT Features 
+	int sID;
+	String sName;
+	@Column(name="sAddress")  //Used to explicitly declare field/column name
+	String sCity;
+	int sAge;
+	
+	
+
+	public Student() {
+		
+	}
+	
+
+	public Student(String sName, String sCity, int sAge) {
+		super();
+		this.sName = sName;
+		this.sCity = sCity;
+		this.sAge = sAge;
+	}
+
+
+	public String getsName() {
+		return sName;
+	}
+
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+
+	public String getsCity() {
+		return sCity;
+	}
+
+	public void setsCity(String sCity) {
+		this.sCity = sCity;
+	}
+
+	public int getsAge() {
+		return sAge;
+	}
+
+	public void setsAge(int sAge) {
+		this.sAge = sAge;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [sName=" + sName + ", sCity=" + sCity + ", sAge=" + sAge + "]";
+	}
+	
+
+}
